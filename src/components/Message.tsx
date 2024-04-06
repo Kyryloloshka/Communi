282dc760a3a426643c17d750109181546f27b38f
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-04-06 21:07:21 +0300

    add pseudo element to message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -16,15 +16,15 @@ function Message({message, myUser, otherUser}: {message: IMessage, myUser: any, 
   const isLastInGroup = !message.isNextMessageSameSender;
   const [open, setOpen] = useState<boolean | undefined>(undefined);
   return (
-    <div key={message.id} className={`flex ${isCurrentUser ? "justify-end" : "justify-start"}`}>
+    <div key={message.id} className={`flex relative ${isCurrentUser ? "justify-end" : "justify-start"}`}>
       {!isCurrentUser &&
-      <div className={`w-10 h-10 mr-2 self-end aspect-square cursor-pointer`}>
+      <div className={`w-8 h-8 mr-2 self-end aspect-square cursor-pointer`}>
         {isLastInGroup && 
         <img src={otherUser.avatarUrl} alt="avatar" className='w-full h-full rounded-full object-cover' />}
       </div>
       }
-      <div className={`text-light-1 max-w-[500px] overflow-hidden flex flex-col flex-wrap ${isCurrentUser ? "bg-dark-5 self-end  rounded-l-xl" : "bg-dark-5 rounded-r-xl self-start"} rounded-sm ${isFirstInGroup && "rounded-t-xl"}`}>
-        {!isCurrentUser && <Link href={"/profile/" + otherUser.id} className="text-primary-500 px-2 pt-1.5 leading-[1em] pb-1">{message.senderName}</Link> }
+      <div className={`text-light-1 pt-1.5 max-w-[500px] overflow-hidden relative flex flex-col gap-1 pb-1 flex-wrap ${isCurrentUser ? "bg-dark-5 self-end  rounded-l-xl" : "bg-dark-5 rounded-r-xl self-start"} rounded-sm ${isFirstInGroup && "rounded-t-xl"} ${isLastInGroup && (isCurrentUser ? "rounded-br-none" : "rounded-bl-none")}`}>
+        {!isCurrentUser && isFirstInGroup && <Link href={"/profile/" + otherUser.id} className="text-primary-500 px-2 leading-[1em]">{message.senderName}</Link> }
         {
           message.image && 
           <Dialog open={open}>
@@ -37,9 +37,12 @@ function Message({message, myUser, otherUser}: {message: IMessage, myUser: any, 
             </DialogContent>
           </Dialog>
         }
-        <p style={{wordBreak: "break-word"}} className='break-words text-sm leading-[1em] px-2 pb-0.5 pt-1.5'>{message.text}</p>
-        <div className={`text-xs  text-gray-400 ${isCurrentUser && "self-end"} px-2 pb-1`}>{getValidTime(message.time)}</div>
+        <p style={{wordBreak: "break-word"}} className='break-words text-sm leading-[1em] px-2'>{message.text}</p>
+        <div className={`text-xs  text-gray-400 ${isCurrentUser && "self-end"} px-2`}>{getValidTime(message.time)}</div>
       </div>
+      {isLastInGroup &&
+        <img className={`w-3 absolute bottom-0 fill-white object-fill ${isCurrentUser ? "-right-3 rotate-90" : "left-[28px]"}`} src="assets/items/pseudo-elem-for-message.svg" alt="" />
+      }
     </div>
   )
 }
